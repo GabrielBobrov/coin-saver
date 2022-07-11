9a00ffccb4c811fd859ae038c6106cf2b7d11902
Matheus <matheustts8@gmail.com>
2022-07-11 19:28:52 -0300

    Create BankAccount and User models

---
+++ CoinSaverAPI/src/CoinSaver.Domain/Entities/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿namespace CoinSaver.Domain.Entities
+{
+    public class BankAccount
+    {
+        /// <summary>
+        /// Id do usuário.
+        /// </summary>
+        public long UserId { get; set; }
+
+        /// <summary>
+        /// Nome do banco.
+        /// </summary>
+        public string BankName { get; set; }
+
+        /// <summary>
+        /// Agência bancária.
+        /// </summary>
+        public string Office { get; set; }
+
+        /// <summary>
+        /// Número da conta sem o dígito.
+        /// </summary>
+        public string AccountNumber { get; set; }
+
+        /// <summary>
+        /// Dígito da conta.
+        /// </summary>
+        public string AccountDigit { get; set; }
+
+        // EF Relations
+
+        public User User { get; set; }
+    }
+}
---
+++ CoinSaverAPI/src/CoinSaver.Domain/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿namespace CoinSaver.Domain.Entities
+{
+    public class User : Base
+    {
+        /// <summary>
+        /// Nome do usuário
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// E-mail do usuário
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Data de criação.
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Bloqueado em.
+        /// </summary>
+        public DateTime? BlockedAt { get; set; }
+
+        // EF Relations
+
+        public List<BankAccount> BankAccounts { get; set; }
+    }
+}
