fe2b549402a435a79bfcf5a9d678487feffd8df4
gabriel <bielbobrov@hotmail.com>
2023-01-13 19:42:25 -0300

    refatorando error messages

--- coinsaver-api/src/main/java/com/coinsaver/core/validation/messages/ErrorMesssages.java
+++
@@ -1,9 +0,0 @@
-package com.coinsaver.core.validation.messages;
-
-public class ErrorMesssages {
-	
-	public static final String USERALREADYEXISTS = "Já existe um usuário cadastrado com o email informado.";
-	public static final String USERNOTFOUND = "Não existe nenhum usuário com o id informado.";
-	
-	public static String userInvalid(String errors) { return "Os campos informados para o usuário estão inválidos" + errors; }
-}
