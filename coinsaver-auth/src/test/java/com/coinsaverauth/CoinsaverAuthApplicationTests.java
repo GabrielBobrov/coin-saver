c6bc4112cf531be034030952e2b568ebef7aab5d
gabriel <bielbobrov@hotmail.com>
2023-03-12 19:03:11 -0300

    add application

---
+++ coinsaver-auth/src/test/java/com/coinsaverauth/CoinsaverAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coinsaverauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinsaverAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
