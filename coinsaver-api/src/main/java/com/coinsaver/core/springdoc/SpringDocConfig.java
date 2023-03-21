0e8fcd549753edc9926098b60a5530a94c1f3a88
gabriel <bielbobrov@hotmail.com>
2023-03-21 19:24:03 -0300

    ajustes

--- coinsaver-api/src/main/java/com/coinsaver/core/springdoc/SpringDocConfig.java
+++ coinsaver-api/src/main/java/com/coinsaver/core/springdoc/SpringDocConfig.java
@@ -1,5 +1,10 @@
 package com.coinsaver.core.springdoc;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -10,8 +15,17 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.Collections;
-
 @Configuration
+@SecurityScheme(name = "security_auth",
+        type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(authorizationCode = @OAuthFlow(
+                authorizationUrl = "${springdoc.oAuthFlow.authorizationUrl}",
+                tokenUrl = "${springdoc.oAuthFlow.tokenUrl}",
+                scopes = {
+                        @OAuthScope(name = "READ", description = "read scope"),
+                        @OAuthScope(name = "WRITE", description = "write scope")
+                }
+        )))
 public class SpringDocConfig {
 
     @Bean
