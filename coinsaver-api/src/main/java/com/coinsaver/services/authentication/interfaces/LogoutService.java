c260a0230d0e6ba506cf4044705d1291c2506992
gabriel <bielbobrov@hotmail.com>
2023-03-22 20:52:04 -0300

    add interface de logout

---
+++ coinsaver-api/src/main/java/com/coinsaver/services/authentication/interfaces/LogoutService.java
@@ -0,0 +1,12 @@
+package com.coinsaver.services.authentication.interfaces;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+
+public interface LogoutService {
+    void logout(HttpServletRequest request,
+                HttpServletResponse response,
+                Authentication authentication);
+
+}
