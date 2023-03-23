9a0e3959e63278f95ae4b52a6716489ac1ac56f1
andre.mostaro1 <andremostaro@outlook.com>
2023-03-23 11:34:38 -0300

    FEAT showModalCadastroPage();

--- coinsaver-front/src/app/components/initial-page/initial-page.component.ts
+++ coinsaver-front/src/app/components/initial-page/initial-page.component.ts
@@ -1,4 +1,6 @@
+import { CadastroPageComponent } from './../cadastro-page/cadastro-page.component';
 import { Component } from '@angular/core';
+import { DialogService } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-initial-page',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./initial-page.component.css']
 })
 export class InitialPageComponent {
+
+  constructor(
+    private dialogService: DialogService,
+  ) {}
+
+  showModalCadastroPage() {
+    const page = this.dialogService.open(CadastroPageComponent, {
+      data: {},
+      width: 'auto',
+    });
+  }
 
 }
