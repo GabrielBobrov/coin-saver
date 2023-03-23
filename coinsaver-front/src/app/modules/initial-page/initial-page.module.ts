1410bf9fbe6228f28fc0edc5030fd8dc6ff3f590
andre.mostaro1 <andremostaro@outlook.com>
2023-03-23 11:34:21 -0300

    FEAT cadastro-page-component; IMPORT DialogService and DynamicDialog;

--- coinsaver-front/src/app/modules/initial-page/initial-page.module.ts
+++ coinsaver-front/src/app/modules/initial-page/initial-page.module.ts
@@ -3,14 +3,23 @@ import { InitialPageRoutingModule } from './initial-page-routing.module';
 import { InitialPageComponent } from './../../components/initial-page/initial-page.component';
 import { NgModule } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { CadastroPageComponent } from '../../components/cadastro-page/cadastro-page.component';
+import { DynamicDialogModule, DialogService } from 'primeng/dynamicdialog';
+import { DialogModule } from 'primeng/dialog';
 
 @NgModule({
   declarations: [
     InitialPageComponent,
+    CadastroPageComponent,
   ],
   imports: [
     InitialPageRoutingModule,
     HttpClientModule,
+    DynamicDialogModule,
+    DialogModule
+  ],
+  providers: [
+    DialogService,
   ]
 })
 
