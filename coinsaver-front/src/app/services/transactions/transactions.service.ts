2144ed64e4c6ab85e071a17bf7e0d467e8b37ea0
andre.mostaro1 <andremostaro@outlook.com>
2023-03-27 10:28:58 -0300

    FEAT TransactionService

--- coinsaver-front/src/app/services/transactions/transactions.service.ts
+++ coinsaver-front/src/app/services/transactions/transactions.service.ts
@@ -1,5 +1,8 @@
-import { HttpClientModule } from '@angular/common/http';
+import { Transaction } from './../../dtos/transactions/transaction.dto';
+import { environment } from './../../environments/environments';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +10,47 @@ import { Injectable } from '@angular/core';
 export class TransactionsService {
 
   constructor(
-    private httpClient: HttpClientModule,
+    private httpClient: HttpClient,
   ) { }
+
+  httpOptions = {
+    headers : new HttpHeaders(
+      {'Content-Type':'application/json'}
+    )
+  };
+
+  baseUrl = environment.api.hostBackend;
+  transactionControllerUrl = environment.api.transactionsControllerBackend;
+
+  getAllTransactions(): Observable<Transaction[]> {
+    return this.httpClient.get<Transaction[]>(
+      `${
+        this.baseUrl +
+        this.transactionControllerUrl +
+        environment.api.backendEndpoints.getTransactions}`
+    )
+    .pipe(
+
+      catchError((erroResponse) => {
+        return throwError(erroResponse);
+      })
+
+    );
+  }
+
+  getMonthTransactions(): Observable<Transaction[]> {
+    return this.httpClient.get<Transaction[]>(
+      `${
+        this.baseUrl +
+        this.transactionControllerUrl +
+        environment.api.backendEndpoints.getTransactionsMonth}`
+    )
+    .pipe(
+
+      catchError((erroResponse) => {
+        return throwError(erroResponse);
+      })
+
+    );
+  }
 }
