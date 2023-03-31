1ce86afff172da6a2c1adbf5f14e127495e0f3a0
andre.mostaro1 <andremostaro@outlook.com>
2023-03-31 16:05:17 -0300

    REFACTOR inicio pagina cadastro

--- coinsaver-front/src/app/components/modal-cadastro/cadastro-page/cadastro-page.component.ts
+++ coinsaver-front/src/app/components/modal-cadastro/cadastro-page/cadastro-page.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-cadastro-page',
@@ -6,5 +15,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastro-page.component.css']
 })
 export class CadastroPageComponent {
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  matcher = new MyErrorStateMatcher();
 
 }
