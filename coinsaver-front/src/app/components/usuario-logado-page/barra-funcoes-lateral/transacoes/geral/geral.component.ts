f72c002d48f73cb58daf57e5fd22f5e3f4283d12
andre.mostaro1 <andremostaro@outlook.com>
2023-04-04 12:07:37 -0300

    FEAT ajustes iniciais paginas geral e mensal;

--- coinsaver-front/src/app/components/usuario-logado-page/barra-funcoes-lateral/transacoes/geral/geral.component.ts
+++ coinsaver-front/src/app/components/usuario-logado-page/barra-funcoes-lateral/transacoes/geral/geral.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-geral',
   templateUrl: './geral.component.html',
-  styleUrls: ['./geral.component.css']
+  styleUrls: ['./geral.component.css', '../../../usuario-logado-page.component.css']
 })
 export class GeralComponent {
 
--- coinsaver-front/src/app/components/usuario-logado-page/barra-funcoes-lateral/transacoes/mensal/mensal.component.ts
+++ coinsaver-front/src/app/components/usuario-logado-page/barra-funcoes-lateral/transacoes/mensal/mensal.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-mensal',
   templateUrl: './mensal.component.html',
-  styleUrls: ['./mensal.component.css']
+  styleUrls: ['./mensal.component.css', '../../../usuario-logado-page.component.css']
 })
 export class MensalComponent {
 
