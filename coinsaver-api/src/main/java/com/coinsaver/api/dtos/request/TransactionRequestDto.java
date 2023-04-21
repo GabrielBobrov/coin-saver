b482b8cc02493f7bdf4af5aa7c19390051389cf2
gabriel <bielbobrov@hotmail.com>
2023-04-21 11:33:31 -0300

    remover paid

--- coinsaver-api/src/main/java/com/coinsaver/api/dtos/request/TransactionRequestDto.java
+++ coinsaver-api/src/main/java/com/coinsaver/api/dtos/request/TransactionRequestDto.java
@@ -37,8 +37,6 @@ public class TransactionRequestDto {
     private TransactionCategoryType category;
     @NotNull
     private Boolean fixedExpense;
-    @NotNull
-    private Boolean paid;
     @Min(2)
     private Integer repeat;
 
