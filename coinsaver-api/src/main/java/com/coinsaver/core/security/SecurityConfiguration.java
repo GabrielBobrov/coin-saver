083a7626c7be13580d09855445220ae5d5d7e8d7
gabriel <bielbobrov@hotmail.com>
2023-05-16 09:25:32 -0300

    äjuste cors

--- coinsaver-api/src/main/java/com/coinsaver/core/security/SecurityConfiguration.java
+++ coinsaver-api/src/main/java/com/coinsaver/core/security/SecurityConfiguration.java
@@ -65,6 +65,7 @@ public class SecurityConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("*");
     }
 }
