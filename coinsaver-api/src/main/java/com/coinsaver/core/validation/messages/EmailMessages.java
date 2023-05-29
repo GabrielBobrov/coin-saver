da257f05afdccf41f850555024e228cc8e27bc6e
andre.mostaro1 <andremostaro@outlook.com>
2023-05-29 09:14:54 -0300

    REFACTOR mensagem de recuperacao de senha com instrucao completa

--- coinsaver-api/src/main/java/com/coinsaver/core/validation/messages/EmailMessages.java
+++ coinsaver-api/src/main/java/com/coinsaver/core/validation/messages/EmailMessages.java
@@ -3,7 +3,7 @@ package com.coinsaver.core.validation.messages;
 public class EmailMessages {
     private EmailMessages() { }
 
-    private static final String RECOVER_PASSWORD = "%s, sua senha atual é: %s.";
+    private static final String RECOVER_PASSWORD = "%s, sua senha atual é: %s. Faça login em sua conta e troque a mesma na aba ALTERAR SENHA.";
 
     public static String getRecoverPasswordMessage(String clientName, String password) {
         return String.format(RECOVER_PASSWORD, clientName, password);
