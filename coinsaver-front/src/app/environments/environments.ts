e94187a86168b5bbe517d7116f9cb64e2bb65ea6
gabriel <bielbobrov@hotmail.com>
2023-06-14 13:27:09 -0300

    alterando host backend para local

--- coinsaver-front/src/app/environments/environments.ts
+++ coinsaver-front/src/app/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   api: {
-    hostBackend: 'https://coin-saver-production.up.railway.app',
+    hostBackend: 'http://localhost:8080',
 
     transactionsControllerBackend: '/transactions',
     transactionsBackendEndpoints: {
